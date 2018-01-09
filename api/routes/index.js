368b35e17d8b5c217c0f48fff693285a6653c430
Navendra Jha <navendra@asu.edu>
2018-01-09 06:31:15 -0700

    Added css files and mvc folder structure

---
+++ api/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var hotelsCntrl = require("../controllers/hotels.controller");
+
+var router = express.Router();
+
+router
+    .route('/hotels')
+    .get(hotelsCntrl.hotelsGetAll);
+
+router
+    .route('/hotels/:hotelId')
+    .get(hotelsCntrl.hotelsGetOne);
+
+router
+    .route('/hotels/new')
+    .post(hotelsCntrl.hotelsAddOne);
+
+
+
+module.exports = router;
+
---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var routes = require("./api/routes");
+var bodyParser = require("body-parser")
+
+app.set("port", 3000);
+
+// app.use(function (req, res, next) {
+//     console.log(req.method, req.url);
+//     next();
+// });
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/api',routes);
+
+// app.get('/json', function (req, res) {
+//     res
+//         .status(200)
+//         .sendFile(path.join(__dirname,"app.js") );
+// });
+
+var server = app.listen(app.get("port"), function () {
+    var port = server.address().port;
+    console.log("Magic happens on port " + port );
+});
